16fdbff3ba66458e2c7b9b5d73deb9fa0819ad69
G1rmmr <blackhand2413@naver.com>
2025-02-10 19:03:04 +0900

    Add : bullet engine

--- dx-tutorial/main.cpp
+++ dx-tutorial/main.cpp
@@ -2,11 +2,15 @@
 
 #include "Game.h"
 
+#include <bullet/btBulletCollisionCommon.h>
+#include <bullet/btBulletDynamicsCommon.h>
+
 const int SCREEN_WIDTH = 1280;
 const int SCREEN_HEIGHT = 720;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int nCmdShow)
 {
+    /*
     core::Game game;
 
     if(!game.Initialize(hInstance, SCREEN_WIDTH, SCREEN_HEIGHT, nCmdShow))
@@ -19,6 +23,24 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE, _In_ LPSTR, _In
 
     game.Run();
     game.Cleanup();
+    */
+
+    btBroadphaseInterface* broadphase = new btDbvtBroadphase();
+    btDefaultCollisionConfiguration* collisionConfig = new btDefaultCollisionConfiguration();
+    btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfig);
+    btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver();
+    btDiscreteDynamicsWorld* world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfig);
+    world->setGravity(btVector3(0, -9.81f, 0));
+
+    world->stepSimulation(1.f / 60.f, 10);
+
+    MessageBox(nullptr, L"Bullet test OK!", L"Error", MB_OK);
+
+    delete world;
+    delete solver;
+    delete dispatcher;
+    delete collisionConfig;
+    delete broadphase;
 
     return 0;
 }
